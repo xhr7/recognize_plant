22e8d4bd1a149dd31ee39906acaf953d0ba19d12
Rahaf <102740867+xhr7@users.noreply.github.com>
2025-05-14 04:58:34 +0300

    Update n.py

--- n.py
+++ n.py
@@ -25,10 +25,10 @@ FILE_ID = "1gD6k2kwNAB-mKgwSkRb8wOzs2NF0xD8w"
 URL = f"https://drive.google.com/uc?id={FILE_ID}"
 
 if not os.path.exists(MODEL_PATH):
-    print("🔽 Downloading DINOv2 model...")
+    print("🔽 Downloading DINOv2 model from Google Drive...")
     gdown.download(URL, MODEL_PATH, quiet=False)
 
-# تحميل نموذج DINOv2 من الملف المحلي
+# تحميل النموذج
 dinov2 = torch.jit.load(MODEL_PATH).eval()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dinov2 = dinov2.to(device)
